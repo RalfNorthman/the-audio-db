5a5999dabbd7c9fa8f7d8a11985fb7c38cdcf5ad
Ralf Northman <ralf.northman@bredband.net>
2018-10-13 19:27:02 +0200

    Working track decoding.

--- src/Main.elm
+++ src/Main.elm
@@ -5,28 +5,30 @@ import Element exposing (..)
 import Browser
 import Http
 import Url.Builder as Url exposing (QueryParameter)
+import Json.Decode as Decode exposing (Decoder, int, string, list)
+import Json.Decode.Pipeline exposing (required, optional, requiredAt)
 
 
 -- Query Types
 
 
-type alias Artist =
+type alias ArtistName =
     String
 
 
-type alias Album =
+type alias AlbumName =
     String
 
 
-type alias Track =
+type alias TrackName =
     String
 
 
 type Query
-    = ArtistSearch Artist
-    | AlbumSearch Artist Album
-    | TrackSearch Artist Track
-    | Discography Artist
+    = ArtistSearch ArtistName
+    | AlbumSearch ArtistName AlbumName
+    | TrackSearch ArtistName TrackName
+    | Discography ArtistName
 
 
 
@@ -97,7 +99,39 @@ request query =
             queryToUrl query
     in
         Http.send Request <|
-            Http.getString url
+            Http.get url trackDecoder
+
+
+
+-- Json
+
+
+type alias TrackSub =
+    { artist : String
+    , album : String
+    , trackNumber : String
+    , track : String
+    }
+
+
+type alias Track =
+    { track : List TrackSub
+    }
+
+
+trackSubDecoder : Decoder TrackSub
+trackSubDecoder =
+    Decode.succeed TrackSub
+        |> required "strArtist" string
+        |> required "strAlbum" string
+        |> required "intTrackNumber" string
+        |> required "strTrack" string
+
+
+trackDecoder : Decoder Track
+trackDecoder =
+    Decode.succeed Track
+        |> required "track" (list trackSubDecoder)
 
 
 
@@ -119,7 +153,21 @@ init _ =
 
 type Msg
     = Submit
-    | Request (Result Http.Error String)
+    | Request (Result Http.Error Track)
+
+
+resultToString result =
+    case List.head result.track of
+        Nothing ->
+            "No Track"
+
+        Just track ->
+            [ track.artist
+            , track.album
+            , track.trackNumber
+            , track.track
+            ]
+                |> String.join " - "
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -128,11 +176,13 @@ update msg model =
         Submit ->
             ( model, request exampleQuery )
 
-        Request (Ok string) ->
-            ( string, Cmd.none )
+        Request (Ok result) ->
+            ( resultToString result
+            , Cmd.none
+            )
 
         Request (Err _) ->
-            ( model, Cmd.none )
+            ( "error", Cmd.none )
 
 
 
